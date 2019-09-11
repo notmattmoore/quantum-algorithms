8177c05d2673fda7e1d8156ff19624b14a738d95
Matthew Moore <matthew.moore@ku.edu>
2019-09-11 17:52:23 -0500

    added distributive lattice

--- simon-semilat/sum.py
+++ simon-semilat/sum.py
@@ -1,29 +1,55 @@
-import ualgebra as UA
+# imports {{{1
 from itertools import *
+from sys import *
+import ualgebra as UA
+#----------------------------------------------------------------------------}}}1
 
-def simon_sum(cong, a, b):
-  exp = lambda x, y: sum([a[i]*x[i] - b[i]*y[i] for i in range(len(x))])
+def simon_sum(cong, a, b): # {{{
+  exp = lambda x, y: sum([a[i]*x[i] + b[i]*y[i] for i in range(len(x))])
   S = 0
   for [s, t] in cong:
     S += (-1) ** (exp(s,t) % 2)
   return S
+#----------------------------------------------------------------------------}}}
 
+# experiment for distributive lattices {{{1
 def meet(x,y):
+  # meet as the bit-wise product
+  return [x[i]*y[i] for i in range(len(x))]
+def join(x,y):
+  # join as the bit-wise truncated sum
+  return [max(x[i],y[i]) for i in range(len(x))]
+
+Ops = []
+Ops.append(UA.Operation(meet, 2, "meet"))
+Ops.append(UA.Operation(join, 2, "join"))
+#----------------------------------------------------------------------------}}}1
+
+n = 4
+A = UA.FancySet( initial=[list(a) for a in list(product([0,1],repeat=n))] ) # {0,1}^n
+Theta, G = UA.rand_cong(A, Ops, MaxNew=len(A), Progress=True)
+
+print("Generators:")
+for g in G:
+  print("  ", g)
+print("Congruence classes:")
+for C in UA.cong_classes(Theta, A):
+  print("  ", C)
+
+print("\na b S")
+#for a,b in product(A,repeat=2):
+#for a,b in combinations(A,2):
+for a in A:
+  S = simon_sum(Theta, a, a)
+  print(a, a, S, "|")
+
+exit()
+
+# experiment for semilattices {{{1
+def meet(x,y):
+  # meet as the bit-wise product
   return [x[i]*y[i] for i in range(len(x))]
 
-M = UA.Operation(meet, 2, "meet")
-
-n = 3
-A = UA.FancySet( initial=[list(a) for a in list(product([0,1],repeat=n))] )
-Theta = UA.rand_cong(A, [M], num_gen=1, Progress=False)
-
-print("Theta has classes:")
-for C in UA.cong_classes(Theta, A):
-  print(C)
-
-print("")
-
-print("a b S | a Theta b?")
-for a,b in combinations(A,2):
-  S = simon_sum(Theta, a, b)
-  print(a, b, S, "|", [a,b] in Theta)
+Ops = []
+Ops.append(UA.Operation(meet, 2, "meet"))
+#----------------------------------------------------------------------------}}}1
