51eb44da8c380b46ca31aa3761a2c8e60b08c837
Matthew Moore <matthew.moore@ku.edu>
2019-09-20 16:53:48 -0500

    added automated testing

--- poly-gen.py
+++ poly-gen.py
@@ -2,6 +2,7 @@
 # a lattice given a certain number of random generators
 
 # imports {{{1
+from itertools import *
 from sys import *
 import ualgebra as UA
 #----------------------------------------------------------------------------}}}1
@@ -18,21 +19,18 @@ Ops = []
 Ops.append(UA.Operation(meet, 2, "meet"))
 Ops.append(UA.Operation(join, 2, "join"))
 
-n = 7
+n = 10
 A = UA.FancySet( initial=[list(a) for a in list(product([0,1],repeat=n))] ) # {0,1}^n
 
 stats = dict()
-total=10**3
-for count in range(total):
-  R, G = UA.rand_subalg(A, Ops, num_gen=n**2, Progress=False)
-  if len(R) not in stats:
-    stats[len(R)] = 0
-  stats[len(R)] += 1
-  stdout.write(str(round(count/total*100, 4)) + "%    \r")
-  if count % 10 == 0: stdout.flush()
-
-print("----------")
-for key in stats:
-  print(key,stats[key]/(count+1)*100)
-print("----------")
+for ng in range(n**2, 0, -1):
+  stats[ng] = 0
+  total=10**3
+  for count in range(total):
+    R, G = UA.rand_subalg(A, Ops, num_gen=ng, Progress=False)
+    if len(R) == len(A):
+      stats[ng] += 1
+    stdout.write("\r" + str(ng) + ": " + str(round(count/total*100, 4)) + "%    ")
+    if count % 10 == 0: stdout.flush()
+  stdout.write("\n  " + "Generators = " + str(ng) + ", success = " + str(round(stats[ng]/total*100, 4)) + "%\n")
 #----------------------------------------------------------------------------}}}1
