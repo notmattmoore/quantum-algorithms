0677c8bbcba794a13d4cb467f8c8572662e99149
Taylor Walenczyk <tawalenczyk@ku.edu>
2019-10-28 17:21:59 -0500

    Added support for clones in poly-gen

--- poly-gen.py
+++ poly-gen.py
@@ -5,6 +5,7 @@
 from itertools import *
 from sys import *
 import ualgebra as UA
+import post_ops as PO
 #----------------------------------------------------------------------------}}}1
 
 # experiment for distributive lattices {{{1
@@ -15,11 +16,32 @@ def join(x,y):
   # join as the bit-wise truncated sum
   return [max(x[i],y[i]) for i in range(len(x))]
 
-Ops = []
-Ops.append(UA.Operation(meet, 2, "meet"))
-Ops.append(UA.Operation(join, 2, "join"))
+Names = [
+"T"    , "P0"    , "P1"    , "P"    , "M"   , "MP0"   , "MP1"   , "MP"   ,
+"MEET" , "MEETP0", "MEETP1", "MEETP", "JOIN", "JOINP0", "JOINP1", "JOINP",
+"D"    , "DP"    , "DM"   , "A"   , "AD"    , "AP0"   , "AP1"   , "AP"   ,
+"U"    , "UD"    , "UM"   , "UP0" , "UP1"   , "F"
+] + [ "T0k for 2 <= k < inf" ] + [ "T0inf" ]   + \
+    [ "PT0k for 2 <= k < inf" ] + [ "PT0inf" ]  + \
+    [ "T1k for 2 <= k < inf" ] + [ "T1inf" ]   + \
+    [ "PT1k for 2 <= k < inf" ] + [ "PT1inf" ]  + \
+    [ "MT0k for 2 <= k < inf" ] + [ "MT0inf" ]  + \
+    [ "MPT0k for 2 <= k < inf" ] + [ "MPT0inf" ] + \
+    [ "MT1k for 2 <= k < inf" ] + [ "MT1inf" ]  + \
+    [ "MPT1k for 2 <= k < inf" ] + [ "MPT1inf" ]
 
-n = 10
+
+print("Possible clones:")
+for name in Names:
+    print("  ", name)
+inp = input("Input a clone name: ")
+print("Clone name", inp, "provided")
+Ops = PO.named_clone(inp)
+print("Corresponding operations:")
+for op in Ops:
+    print("  ", op.name) 
+
+n = 5 
 A = UA.FancySet( initial=[list(a) for a in list(product([0,1],repeat=n))] ) # {0,1}^n
 
 stats = dict()
